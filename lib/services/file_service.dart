c902395937f9a56a5d18e2b883bfcd0e3c24646d
Ruben Falcao <79042516+RubenFalcao14@users.noreply.github.com>
2025-02-18 09:24:02 +0530

    18/2/25

--- lib/services/file_service.dart
+++ lib/services/file_service.dart
@@ -1,7 +1,30 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:nlp/utils/snackbar_utils.dart';
 
 class FileService {
   final TextEditingController titleController = TextEditingController();
 
   bool fieldsNotEmpty = false;
+
+  File? _selectedFiled;
+  String _selectedDirectory = '';
+  
+  void savedContent(context) async{
+    final title = titleController.text;
+
+    final textContent = "Title:\n\n$title";
+
+    try{
+      if(_selectedFiled != null){
+        await _selectedFiled!.writeAsString(textContent);
+      } else{
+        
+      }
+
+  }catch(e){
+    SnackBarUtils.showSnackbar(context, Icons.error, 'File not saved');
+  }
+  }
+
 }
